e7a8ab93517398e24da42ef87db409939e1b6d2a
marvin <sea-son@foxmail.com>
2024-10-10 11:51:11 +0800

    fix: 滚动条

--- packages/react-ai/src/main.tsx
+++ packages/react-ai/src/main.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Button} from "../";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Button>12312</Button>
     </React.StrictMode>,
 )
