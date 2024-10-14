94928de28c55ffd7db7a8a05ee1f5197370a1a9f
marvin <sea-son@foxmail.com>
2024-10-14 18:03:00 +0800

    fix: vite 配置优化 preserveModules: true,                        preserveModulesRoot: "src",

--- packages/react-ai/vite.config.ts
+++ packages/react-ai/vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
-// The glob library helps you to specify a set of filenames. In this case it selects all files ending with .ts or .tsx
-import { glob } from "glob";
-import { fileURLToPath } from "node:url";
-import { extname, relative, resolve } from "path";
+import { resolve } from "path";
 
 console.log("🚀  ", resolve(__dirname, "lib"));
 // https://vitejs.dev/config/
@@ -31,17 +28,9 @@ export default defineConfig({
     copyPublicDir: false,
     rollupOptions: {
       external: (id) => !id.startsWith(".") && !id.startsWith("/") && !id.startsWith("@/"),
-      input: Object.fromEntries(
-        glob.sync("lib/**/*.{ts,tsx}").map(file => [
-          // The name of the entry point
-          // lib/nested/foo.ts becomes nested/foo
-          relative("lib", file.slice(0, file.length - extname(file).length)),
-          // The absolute path to the entry file
-          // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-          fileURLToPath(new URL(file, import.meta.url)),
-        ]),
-      ),
       output: {
+        preserveModules: true,
+        preserveModulesRoot: "src",
         assetFileNames: "assets/[name][extname]",
         entryFileNames: (chunkInfo) => `${chunkInfo.name}.js`,
       },
