46121252b27ea7a9b460ad5700328a723f43cff3
marvin <sea-son@foxmail.com>
2024-10-29 17:16:27 +0800

    fix: chat conversation dialog

--- apps/ai-chat-frontend/src/views/Chat/index.jsx
+++ apps/ai-chat-frontend/src/views/Chat/index.jsx
@@ -1,8 +1,9 @@
-import {Chat} from "@ai-chat/chat";
-import {useChatApproach, useChatPage} from "./hooks/index.js";
+import { Chat } from "@ai-chat/chat";
+import { useChatApproach, useChatPage } from "./hooks/index.js";
 import ConversationBar from "./components/ConversationBar.jsx";
 import useChatExtend from "./hooks/useChatExtend.jsx";
-import { Logout } from "@icon-park/react";
+import { CloseOne, Logout, MenuFoldOne } from "@icon-park/react";
+import { useRef } from "react";
 
 export default function ChatPage() {
     const {
@@ -17,29 +18,44 @@ export default function ChatPage() {
         conversations,
     })
 
+    const dialogRef = useRef(null);
+
 
     return <>
+        <dialog ref={dialogRef} className={"p-4 rounded-lg focus:outline-none"}>
+            <div className={"relative flex justify-center h-12 cursor-pointer"}>
+                <div>
+                    {'会话列表'}
+                </div>
+                <CloseOne className={"absolute right-0"} onClick={() => {
+                    dialogRef.current.close();
+                }} />
+            </div>
+            <div className={"flex-grow overflow-y-scroll bg-gray-100 p-2"} onClick={() => dialogRef.current.close()}>
+                <ConversationBar
+                    checkoutConversation={chatProps.checkoutConversation}
+                    fetchConversations={fetchConversations}
+                    conversations={conversations}
+                    conversationId={chatProps.conversationId}
+                    setHistoryMessages={chatProps.setHistoryMessages}
+                    fetchConversationMessages={fetchConversationMessages}
+                    deleteConversation={deleteConversation}
+                />
+            </div>
+        </dialog>
         <div className={"bg-gradient-to-r from-green-300 to-blue-500  h-screen flex flex-col items-center gap-[1px]"}>
-            <div className={"flex-shrink-0 px-4 h-12 bg-white w-full flex justify-end items-center"}>
+            <div className={"flex-shrink-0 px-4 h-12 bg-white w-full flex justify-between items-center gap-4"}>
+                <MenuFoldOne className={"cursor-pointer"} onClick={() => {
+                    dialogRef.current.showModal();
+                }} />
                 <Logout className={"cursor-pointer"} onClick={() => {
                     sessionStorage.removeItem("token");
                     window.location.reload();
-                }}/>
+                }} />
             </div>
             <div className={"flex-grow bg-[#fff] w-[100%] p-8 flex gap-4 justify-center overflow-y-scroll"}>
-                <div className={"p-4 border rounded-xl bg-gray-400 b flex flex-col w-[300px]"}>
-                    <ConversationBar
-                        checkoutConversation={chatProps.checkoutConversation}
-                        fetchConversations={fetchConversations}
-                        conversations={conversations}
-                        conversationId={chatProps.conversationId}
-                        setHistoryMessages={chatProps.setHistoryMessages}
-                        fetchConversationMessages={fetchConversationMessages}
-                        deleteConversation={deleteConversation}
-                    />
-                </div>
-                <div className={"flex-[3] w-[50%] border rounded-xl"}>
-                    <Chat {...chatProps}/>
+                <div className={"flex-grow rounded-xl"}>
+                    <Chat {...chatProps} />
                 </div>
                 {/*<div className={"flex-1 border rounded-xl p-4 text-white bg-gray-400 flex flex-col"}>*/}
                 {/*    <EvalPanel state={approachHandle.state}/>*/}
