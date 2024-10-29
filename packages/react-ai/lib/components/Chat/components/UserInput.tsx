a47d4cb97e4b7006343406ee8f491a2e5c119429
marvin <sea-son@foxmail.com>
2024-10-29 17:39:05 +0800

    fix: chat 组件不完善的功能屏蔽

--- packages/react-ai/lib/components/Chat/components/UserInput.tsx
+++ packages/react-ai/lib/components/Chat/components/UserInput.tsx
@@ -24,23 +24,23 @@ export const UserInput = () => {
       alert("消息不能为空");
     }
   };
-  return <div className={"flex flex-col gap-4 border p-4 rounded-lg"}>
+  return <div className={""}>
     <AttachmentLayout />
 
     <div className={"flex item-center gap-2 "}>
-      <FileSelector
-        onChange={(files) => {
-          onAction(ChatActionType.SelectAttachment, {
-            attachments: Array.from(files).map(file => {
-              return {
-                id: file.name,
-                type: "file",
-                value: file,
-              } as Attachment
-            }),
-          });
-        }}>上传
-      </FileSelector>
+      {/*<FileSelector*/}
+      {/*  onChange={(files) => {*/}
+      {/*    onAction(ChatActionType.SelectAttachment, {*/}
+      {/*      attachments: Array.from(files).map(file => {*/}
+      {/*        return {*/}
+      {/*          id: file.name,*/}
+      {/*          type: "file",*/}
+      {/*          value: file,*/}
+      {/*        } as Attachment*/}
+      {/*      }),*/}
+      {/*    });*/}
+      {/*  }}>上传*/}
+      {/*</FileSelector>*/}
 
       <form className={"relative flex-grow flex gap-4"} action="#" onSubmit={e => {
         e.preventDefault();
@@ -49,7 +49,7 @@ export const UserInput = () => {
         <div ref={reactorRef} className={"absolute left-0 right-0 bottom-[48px] z-10"}></div>
         <input
           ref={triggerRef}
-          className={"border w-full px-4 py-2 rounded resize-none"}
+          className={"border w-full px-4 py-2 rounded-lg resize-none"}
           value={value}
           onChange={(e) => {
             setValue(e.target.value);
